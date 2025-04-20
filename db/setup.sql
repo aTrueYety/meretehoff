ee4a73b9fb4c7a013afc4fcb7b224eb2d1a3a986
aTrueYety <erikhoff00@gmail.com>
2025-04-20 21:37:38 +0200

    update er diagram and setup.sql

--- db/setup.sql
+++ db/setup.sql
@@ -1,35 +1,67 @@
-CREATE TABLE Users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE user (
+    id CHAR(36) PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Paintings (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE image (
+    id CHAR(36) PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    file_path VARCHAR(255) NOT NULL,
+    file_path VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE painting (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255),
     price DECIMAL(10, 2),
+    is_sold BOOLEAN DEFAULT FALSE,
     description TEXT,
     size_v FLOAT,
     size_h FLOAT,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    finished_at DATE
 );
 
-CREATE TABLE Collections (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE collection (
+    id CHAR(36) PRIMARY KEY,
+    name VARCHAR(255),
     description TEXT,
     position INT,
-    started_at DATETIME NOT NULL,
-    ended_at DATETIME
+    started_at DATE,
+    finished_at DATE
 );
 
-CREATE TABLE CollectionPaintings (
-    painting_id INT NOT NULL,
-    collection_id INT NOT NULL,
+CREATE TABLE exhibition (
+    id CHAR(36) PRIMARY KEY,
+    location VARCHAR(255),
+    description TEXT
+    position INT,
+    started_at DATE,
+    finished_at DATE
+);
+
+CREATE TABLE painting_image (
+    image_id CHAR(36),
+    painting_id CHAR(36),
+    position INT,
+    PRIMARY KEY (image_id, painting_id),
+    FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE CASCADE,
+    FOREIGN KEY (painting_id) REFERENCES painting(id) ON DELETE CASCADE
+);
+
+CREATE TABLE exhibition_image (
+    image_id CHAR(36),
+    exhibition_id CHAR(36),
+    position INT,
+    PRIMARY KEY (image_id, exhibition_id),
+    FOREIGN KEY (image_id) REFERENCES image(id) ON DELETE CASCADE,
+    FOREIGN KEY (exhibition_id) REFERENCES exhibition(id) ON DELETE CASCADE
+);
+
+CREATE TABLE collection_painting (
+    painting_id CHAR(36),
+    collection_id CHAR(36),
     position INT,
     PRIMARY KEY (painting_id, collection_id),
-    FOREIGN KEY (painting_id) REFERENCES Paintings(id) ON DELETE CASCADE,
-    FOREIGN KEY (collection_id) REFERENCES Collections(id) ON DELETE CASCADE
+    FOREIGN KEY (painting_id) REFERENCES painting(id) ON DELETE CASCADE,
+    FOREIGN KEY (collection_id) REFERENCES collection(id) ON DELETE CASCADE
 );
